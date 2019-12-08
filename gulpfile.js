ba312d3ae8d8688c735d9e12740515c178a2e81c
Didiq <dchernov722@gmail.com>
2019-12-08 16:35:19 +0300

    fix: rewrite gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
 
 // Static server
 function bs() {
   serveSass();
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
-  watch("./*.html").on('change', browserSync.reload);
+  watch("./.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
-  watch("./js/*.js").on('change', browserSync.reload);
+  watch("./js/.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-  return src("*./sass/*.sass")
-      .pipe(sass())
-      .pipe(dest("./css"))
-      .pipe(browserSync.stream());
+  return src("./sass/*.sass")
+    .pipe(sass())
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
